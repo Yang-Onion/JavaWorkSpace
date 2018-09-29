f054539d9519fe7d9aa21f2fdef26df33f47fc53
Yang-Onion <MyOnion@126.com>
2018-09-29 16:28:05 +0800

    09.SpringBoot-Redis initial

--- springboot/08.SpringBoot-Thymeleaf/src/main/java/com/yangonion/thymeleaf/controller/IndexController.java
+++ springboot/08.SpringBoot-Thymeleaf/src/main/java/com/yangonion/thymeleaf/controller/IndexController.java
@@ -4,6 +4,8 @@ import com.yangonion.thymeleaf.bean.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +13,7 @@ import java.util.List;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/home")
     public String index(Model model){
         List<User> list = new ArrayList<User>();
         list.add(new User(1,"Yang-Onion"));
