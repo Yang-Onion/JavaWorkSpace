60c6f00d9f5abf8c799ca9a9e3de33c4d64fcc7c
Yang-Onion <MyOnion@126.com>
2018-10-10 12:03:32 +0800

    controller test

--- springboot/11.SpringBoot-Test/src/test/java/com/yangonion/test/UserControllerTest.java
+++ springboot/11.SpringBoot-Test/src/test/java/com/yangonion/test/UserControllerTest.java
@@ -1,4 +1,42 @@
 package com.yangonion.test;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setupMockMvc(){
+        mockMvc= MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void queryUserTest() throws Exception{
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/queryUser?id=1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Yang-Onion"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+
 }
