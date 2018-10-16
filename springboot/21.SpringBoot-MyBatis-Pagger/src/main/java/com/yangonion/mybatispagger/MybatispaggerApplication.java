639b1a79a28df8c4c652dcf9ff36408d987f99b0
Yang-Onion <MyOnion@126.com>
2018-10-16 15:43:27 +0800

    init SpringBoot-Shiro and SpringBoot-Security

---
+++ springboot/21.SpringBoot-MyBatis-Pagger/src/main/java/com/yangonion/mybatispagger/MybatispaggerApplication.java
@@ -0,0 +1,12 @@
+package com.yangonion.mybatispagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatispaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatispaggerApplication.class, args);
+	}
+}
---
+++ springboot/21.SpringBoot-MyBatis-Pagger/src/main/java/com/yangonion/mybatispagger/model/User.java
@@ -0,0 +1,11 @@
+package com.yangonion.mybatispagger.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 2178483721846556014L;
+    private  String id;
+    private  String name;
+
+}
