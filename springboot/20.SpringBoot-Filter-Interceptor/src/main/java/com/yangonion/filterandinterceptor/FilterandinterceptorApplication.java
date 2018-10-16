a850509aaceef062ee565cc85abc4009f756771e
Yang-Onion <MyOnion@126.com>
2018-10-16 10:05:13 +0800

    20.SpringBoot-Filter-Interceptor initial

---
+++ springboot/20.SpringBoot-Filter-Interceptor/src/main/java/com/yangonion/filterandinterceptor/FilterandinterceptorApplication.java
@@ -0,0 +1,12 @@
+package com.yangonion.filterandinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterandinterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterandinterceptorApplication.class, args);
+	}
+}
---
+++ springboot/20.SpringBoot-Filter-Interceptor/src/test/java/com/yangonion/filterandinterceptor/FilterandinterceptorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yangonion.filterandinterceptor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilterandinterceptorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
